9b2435afe04d8eb07eb81a5ac649e0a75c8311ef
Grco99 <grga2906@gmail.com>
2026-01-27 20:57:12 +0100

    JS vjezba 01

---
+++ 2JS/vjezba01/skripta.js
@@ -0,0 +1,20 @@
+console.info('Hello world 6');  
+console.table({ime:"Pero", prezime:"Perić   "});
+document.getElementById('ispis').innerText='Hello World 7';
+document.getElementById('ispis').style.color='red';
+document.getElementById('ue').innerHTML='<a href="#">Hello World 8</a>';
+console.log('Osnovna škola "Ivana Filipovića"');
+
+console.log('Mc\'Donalds'); //ALTGR+Q\
+console.log("Mc 'Donalds");
+
+// što ako želim u konzolu ispisati\
+console.log('onda radim //ovo');
+
+console.log(`
+        Ovo je moj
+        tekst
+        kroz više redova`
+);
+
+
